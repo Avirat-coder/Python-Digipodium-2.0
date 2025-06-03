b17c0bbf8cbc0f94166912549f903b5d2afd8e52
Avirat-coder <aviratpal2004@gmail.com>
2025-06-04 04:15:23 +0530

    Logical and analytical thinking

---
+++ armstrong.py
@@ -0,0 +1,12 @@
+num= int((input("Enter a number:  ")))
+original=num
+sum=0
+
+while(num>0):
+    digit=num%10
+    sum=sum+(digit**3)
+    num=num//10
+if (original==sum) :
+    print(f"{original} is a Armstrong")
+else:
+    print(f"{original} is not a Not a Armstrong")
---
+++ factorial.py
@@ -0,0 +1,6 @@
+a=int(input("Enter the number"))
+sum=1
+while(a>0):
+    sum*=a
+    a-=1
+print(sum)    
